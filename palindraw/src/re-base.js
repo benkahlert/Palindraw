338caa08162ba9e9877dae957662ec5220ac4c04
Ben Kahlert <bkahlert@purdue.edu>
2018-04-09 23:50:53 -0400

    Added re-base and react-router-dom: npm install --save react-router-dom and npm install --save re-base

---
+++ palindraw/src/re-base.js
@@ -0,0 +1,23 @@
+// Import rebase, database, and auth
+import Rebase from "re-base";
+import firebase from "firebase/app";
+import database from "firebase/database";
+import "firebase/auth";
+
+// App object for firebase configuration
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyAE-fBFUasyYdDSaZt7kl-JLXGPz7kY9kQ",
+    authDomain: "palindraw.firebaseapp.com",
+    databaseURL: "https://palindraw.firebaseio.com",
+    projectId: "palindraw",
+    storageBucket: "",
+    messagingSenderId: "639443156123"
+});
+
+// const db = database(app);
+const rebase = Rebase.createClass(app.database());
+
+// export app, google, or base defaultly
+export const auth = app.auth(); 
+export const google = new firebase.auth.GoogleAuthProvider();
+export default rebase;
